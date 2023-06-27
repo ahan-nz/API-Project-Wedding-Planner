16cd6bc7fe713ca5e5fc54375af85c543a057518
ahan-nz <14235@coderacademy.edu.au>
2023-06-27 12:01:07 +1000

    installed flask-jwt-extended

--- src/blueprints/auth_bp.py
+++ src/blueprints/auth_bp.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models.user import User, UserSchema
+from init import db, bcrypt
+from sqlalchemy.exc import IntegrityError
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -8,3 +10,21 @@ def all_users():
     stmt = db.select(User)
     users = db.session.scalars(stmt)
     return UserSchema(many=True, exclude=['password']).dump(users)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    try:
+        user_info = UserSchema().load(request.json)
+
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+            name=user_info['name']
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
