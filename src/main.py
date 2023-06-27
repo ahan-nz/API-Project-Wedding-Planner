11794486e029c51d1c4870aa06f09567a9cfb7ac
ahan-nz <14235@coderacademy.edu.au>
2023-06-27 12:03:48 +1000

    added jwt secret key

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ def setup():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
 
     return app
