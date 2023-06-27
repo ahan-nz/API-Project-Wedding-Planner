5b6a3ddcfe33c3448724ec2838724b174aff883e
ahan-nz <14235@coderacademy.edu.au>
2023-06-27 23:32:55 +1000

    installed python-dotenv

--- src/blueprints/auth_bp.py
+++ src/blueprints/auth_bp.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 from models.user import User, UserSchema
 from init import db, bcrypt
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -28,3 +30,23 @@ def register():
         return UserSchema(exclude=['password']).dump(user), 201
     except IntegrityError:
         return {'error': 'Email address already in use'}, 409
+    
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    try:
+        stmt = db.select(User).filter_by(email=request.json['email'])
+        user = db.session.scalar(stmt)
+        if user and bcrypt.check_password_hash(user.password, request.json['password']):
+            token = create_access_token(identity=user.id, expires_delta=timedelta(hours=12))
+            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
+        else:
+            return {'error': 'Invalid email address or password'}, 401
+    except KeyError:
+        return {'error': 'Email and password are required'}, 400
+    
+def admin_required():
+  user_id = get_jwt_identity()
+  stmt = db.select(User).filter_by(id=user_id)
+  user = db.session.scalar(stmt)
+  if not (user and user.is_admin):
+    abort(401, description="You must be an admin")
